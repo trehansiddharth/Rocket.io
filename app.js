aff4d81722f0631118c981870577332740c515f9
Siddharth Trehan <trehansiddharth@gmail.com>
2013-12-02 01:38:12 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,267 @@
+var http	= require("http"),
+	url		= require("url"),
+	fs		= require("fs"),
+	express	= require("express"),
+	connect = require("connect"),
+	JSZip	= require("node-zip"),
+	dl		= require("delivery");
+
+var port = parseInt(process.env.PORT, 10) || 8080;
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+var opendocuments = [];
+io.sockets.on('connection', function (socket) {
+	/*delivery = dl.listen(socket);
+	delivery.on('receive.success', function(file) {
+		fs.writeFile("./p/" + socket.projid + "/" + file.name, file.buffer, function (err) {});
+	});*/
+	socket.on('create', function (projid) {
+		socket.username = random_username();
+		console.log(socket.username);
+		socket.projid = projid;
+		socket.document = null;
+		socket.join(projid);
+		var files = fs.readdirSync("./p/" + projid + "/");
+		files.splice(files.indexOf("build"), 1);
+		socket.emit('update-files', files);
+		socket.emit('update-username', socket.username);
+		socket.broadcast.in(socket.projid).emit('user-online', socket.username);
+		console.log(socket.username);
+	});
+	socket.on('get-file', function (filename) {
+		var contents = fs.readFileSync("./p/" + socket.projid + "/" + filename, "utf8");
+		socket.emit('result-contents', contents);
+		if (socket.document != null)
+		{
+			socket.emit('get-whole-file');
+			socket.leave(socket.document);
+		}
+		socket.document = socket.projid + "/" + filename;
+		socket.join(socket.document);
+		if (!contains(opendocuments, socket.document))
+		{
+			opendocuments.push(socket.document);
+		}
+	});
+	socket.on('file-change', function(change) {
+		socket.broadcast.in(socket.document).emit('file-change', change);
+	});
+	socket.on('file-save', function (contents) {
+		path = "./p/" + socket.document;
+		fs.writeFile(path, contents);
+	});
+	socket.on('disconnect', function () {
+		if (socket.document != null)
+		{
+			socket.leave(socket.document);
+		}
+		socket.leave(socket.projid);
+		io.sockets.in(socket.projid).emit('user-offline', socket.username);
+	});
+	socket.on('make-file', function (filename, contents) {
+		var projid = socket.projid;
+		var filename = filename;
+		var path = "./p/" + projid + "/" + filename;
+		console.log(path);
+		fs.openSync(path, "w");
+		fs.writeFileSync(path, contents);
+		io.sockets.in(projid).emit('update-files', [filename]);
+	});
+	socket.on('send-chat', function (message) {
+		io.sockets.in(socket.projid).emit('chat', socket.username, message);
+	});
+});
+var tid = setInterval(function () {
+	for (var i = 0; i < opendocuments.length; i++)
+	{
+		var document = opendocuments[i];
+		var clients = io.sockets.clients(document);
+		if (clients.length > 0)
+		{
+			clients[0].emit('get-whole-file');
+		}
+		else
+		{
+			opendocuments.splice(opendocuments.indexOf(document), 1);
+		}
+	}
+}, 3000);
+
+app.configure(function () {
+  app.use(connect.bodyParser());
+  app.use(app.router);
+  express.json();
+  express.urlencoded();
+});
+
+server.listen(port);
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+	var redirect_url = "";
+	
+	while (url_exists(redirect_url))
+	{
+		redirect_url = random_url();
+	}
+	response.redirect(redirect_url);
+	//response.sendfile("./index.html");
+});
+
+app.post('/d/upload', function(request, response) {
+	var parsed = request.headers.referer.split("/");
+	var projid = parsed[parsed.length - 1];
+	var msg = request.body[Object.keys(request.body)[0]];
+	var start = msg.indexOf("\r\n\r\n") + 4;
+	var stop = msg.lastIndexOf("\n\r\n");
+	var contents = msg.substr(start, stop - start);
+	start = msg.indexOf("filename=\"") + 10;
+	stop = msg.indexOf("\"\r\nContent-Type:");
+	var filename = msg.substr(start, stop - start);
+	var path = "./p/" + projid + "/" + filename;
+	console.log(path);
+	fs.openSync(path, "w");
+	fs.writeFileSync(path, contents);
+	io.sockets.in(projid).emit('update-files', [filename]);
+	response.end();
+});
+
+app.use(function(req, res, next) {
+	if (req.path.substring(0, 3) == '/p/')
+	{
+		var exists = fs.existsSync('.' + req.path);
+		if (exists)
+		{
+			res.sendfile('./snapcode.html');
+		}
+		else
+		{
+			fs.mkdirSync('.' + req.path);
+			fs.mkdirSync('.' + req.path + '/build');
+			//fs.writeFileSync('.' + req.path + '/project.txt', '');
+			res.sendfile('./snapcode.html');
+		}
+	}
+	else if (req.path.substring(0, 3) == '/f/')
+	{
+	
+	}
+	else if (req.path.substring(0, 3) == '/g/')
+	{
+	
+	}
+	else if (req.path.substring(0, 3) == '/r/')
+	{
+	
+	}
+	else if (req.path.substring(0, 11) == '/d/download')
+	{
+		var parsed = req.headers.referer.split("/");
+		var projid = parsed[parsed.length - 1];
+		var path = "./p/" + projid;
+		writeZip(path, projid);
+		var file = fs.readFileSync('./downloads/' + projid + '.zip', 'binary');
+		//res.set('Content-Disposition', 'attachment; filename=' + projid + '.zip');
+		//res.set('Content-Type', 'application/zip');
+		//res.set('Content-Length', file.length);
+		res.attachment();
+		//res.end(file, 'binary');
+		res.pipe(file);
+		//res.sendfile(file);
+	}
+	else
+	{
+		next();
+	}
+});
+
+console.log("Listening on port " + port + " ...");
+
+random_url = function () {
+	var result = "/p/";
+	var possible = "abcdefghijklmnopqrstuvwxyz0123456789"
+	
+	for (var i = 0; i < 8; i++)
+	{
+		result += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	
+	return result;
+};
+url_exists = function(url) {
+	if (url == "")
+	{
+		return true;
+	}
+	else
+	{
+		return fs.existsSync('.' + url);
+	}
+}
+writeZip = function(dir,name) {
+	var zip = new JSZip(),
+		code = zip.folder(dir),
+		filename = name + '.zip';
+		
+	var options = {base64: false, compression:'DEFLATE'};
+	var output = zip.generate(options);
+	fs.writeFileSync('./downloads/' + filename, output, 'binary');
+	console.log('creating ' + filename);
+};
+
+var adjectives = fs.readFileSync('./accessory/adjectives.txt').toString().split("\n");
+var nouns = fs.readFileSync('./accessory/nouns.txt').toString().split("\n");
+
+random_username = function () {
+	random1 = Math.floor(Math.random() * adjectives.length);
+	random2 = Math.floor(Math.random() * nouns.length);
+	return adjectives[random1] + " " + nouns[random2];
+};
+function get_line(filename, line_no, callback) {
+    var stream = fs.createReadStream(filename, {
+      flags: 'r',
+      encoding: 'utf-8',
+      fd: null,
+      mode: 0666,
+      bufferSize: 64 * 1024
+    });
+
+    var fileData = '';
+    stream.on('data', function(data){
+      fileData += data;
+
+      // The next lines should be improved
+      var lines = fileData.split("\n");
+
+      if(lines.length >= +line_no){
+        stream.destroy();
+        callback(null, lines[+line_no]);
+      }
+    });
+
+    stream.on('error', function(){
+      callback('Error', null);
+    });
+
+    stream.on('end', function(){
+      callback('File end reached without finding line', null);
+    });
+
+}
+function is_valid_filename(filename) {
+	return true;
+}
+
+function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+       if (a[i] === obj) {
+           return true;
+       }
+    }
+    return false;
+}
---
+++ p/rp9xnwmw/snapcode.js
@@ -0,0 +1,210 @@
+$(document).ready(function ()
+{
+	var editor = ace.edit("editor");
+	editor.setValue("", -1);
+	editor.setFontSize(15);
+	var uploading = true;
+	$("#upload").on('dragenter', function (e) 
+	{
+		if (uploading)
+		{
+			e.stopPropagation();
+			e.preventDefault();
+			$(this).css('border-color', 'black');
+		}
+	});
+	$("#upload").on('dragover', function (e) 
+	{
+		if (uploading)
+		{
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	});
+	$("#upload").on('drop', function (e) 
+	{
+		if (uploading)
+		{
+			$(this).css('border-color', 'gray');
+			e.preventDefault();
+			var files = e.originalEvent.dataTransfer.files;
+		 
+			//We need to send dropped files to Server
+			handleFileUpload(files, $("#upload"));
+		}
+	});
+	$(document).on('dragenter', function (e) 
+	{
+		if (uploading)
+		{
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	});
+	$(document).on('dragover', function (e) 
+	{
+		if (uploading)
+		{
+			e.stopPropagation();
+			e.preventDefault();
+			$("#upload").css('border-color', 'gray');
+		}
+	});
+	$(document).on('drop', function (e) 
+	{
+		if (uploading)
+		{
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	});
+	
+	$("#choosefile").on('click', function (e)
+	{
+		if (uploading)
+		{
+			alert("Not yet supported!");
+		}
+	});
+	
+	var url = $(location).attr('href');
+	
+	if (url.indexOf("/p/") !== -1)
+	{
+		var parsed = $(location).attr('href').split('/');
+		var projid = parsed[parsed.length - 1];
+		var hostid = parsed[2];
+		
+		socket = io.connect("http://" + hostid);
+		
+		/*socket.on('connect', function () {
+			console.log(require("delivery"));
+			delivery = new require("delivery/lib/client").Delivery(socket);
+		});*/
+		
+		console.log(hostid);
+	
+		$(".add-file").click(function ()
+		{
+			$(".item").removeClass("selected");
+			$(this).addClass("selected");
+			$("#editor").addClass("invisible-element");
+			$("#start").removeClass("invisible-element");
+		});
+		
+		var gotofile = true;
+		var hasfiles = false;
+		
+		$("#makeproject").click(function ()
+		{
+			if ($("#filename").val().indexOf(".") != -1)
+			{
+				socket.emit('make-file', $("#filename").val(), $("#cpfile").val());
+				//console.log($("#new-filename").val());
+				gotofile = true;
+				$("#filename").val("");
+				$("#cpfile").val("");
+			}
+			else
+			{
+				alert("Your file name must be non-empty and specify an extension.");
+			}
+		});
+		
+		socket.on('update-files', function (files) {
+			if (files.length > 0)
+			{
+				$("#editor").removeClass("invisible-element");
+				$("#start").addClass("invisible-element");
+				$("#empty").addClass("invisible-element");
+				$("#notempty").removeClass("invisible-element");
+			}
+			else
+			{
+				$("#editor").addClass("invisible-element");
+				$("#start").removeClass("invisible-element");
+			}
+			for (var i = 0; i < files.length; i++)
+			{
+				var newid = files[i].replace(" ", "_");
+				$(".super-select").prepend("<div class=\"item\" id=\"" + newid + "\"><span class=\"glyphicon glyphicon-file\"></span> " + files[i] + "</div>");
+				console.log("#" + newid);
+				$(".item#" + newid.replace(".", "\\.")).click(function ()
+				{
+					$(".item").removeClass("selected");
+					$(this).addClass("selected");
+					//uploading = false;
+					socket.emit('get-file', $(this).attr('id'));
+					$("#editor").removeClass("invisible-element");
+					$("#start").addClass("invisible-element");
+					var modename = require("ace/ext/modelist").getModeForPath($(this).attr('id')).mode;
+					console.log(modename);
+					var modeobj = require(modename).Mode;
+					editor.getSession().setMode(new modeobj());
+				});
+				if (i == files.length - 1 && gotofile)
+				{
+					$(".item#" + newid.replace(".", "\\.")).trigger("click");
+					gotofile = false;
+				}
+			}
+		});
+		
+		var inserted = false;
+		
+		socket.on('result-contents', function (contents) {
+			inserted = true;
+			editor.setValue(contents, -1);
+			inserted = false;
+		});
+		
+		socket.on('file-change', function (change) {
+			inserted = true;
+			editor.getSession().getDocument().applyDeltas([change]);
+			inserted = false;
+		});
+		
+		socket.on('get-whole-file', function () {
+			socket.emit('file-save', editor.getSession().getValue());
+		});
+		
+		editor.on('change', function (e) {
+			if (!inserted)
+			{
+				socket.emit('file-change', e.data);
+			}
+		});
+		
+		socket.emit('create', projid);
+	}
+	
+	$("#download").click(function () {
+		$.ajax({
+			url: "/d/download"
+		});
+	});
+});
+function handleFileUpload(files,obj)
+{
+   for (var i = 0; i < files.length; i++) 
+   {
+        var fd = new FormData();
+	    var uploadURL = "/d/upload";
+		fd.append('file', files[i]);
+	    $.ajax({
+	        url: uploadURL,
+	        type: "POST",
+	        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+	        processData: false,
+	        data: fd
+	    });
+   }
+}
+
+/*function handleFileUpload(files, obj)
+{
+   for (var i = 0; i < files.length; i++) 
+   {
+        delivery.send(files[i]);
+   }
+}*/
